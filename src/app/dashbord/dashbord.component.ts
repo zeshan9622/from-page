50685486321eff71eaf05b83c379dbc6ad0c53ae
zeshan9622 <zeshanchaudhary09@gmail.com>
2020-07-28 13:53:30 +0530

    dashboard

--- src/app/dashbord/dashbord.component.ts
+++ src/app/dashbord/dashbord.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import  *  as  data  from  './data.json';
 @Component({
   selector: 'app-dashbord',
   templateUrl: './dashbord.component.html',
@@ -9,7 +9,15 @@ export class DashbordComponent implements OnInit {
 
   constructor() { }
 
+dashbordData
+purchase
+Product
   ngOnInit() {
+    this.dashbordData= data["default"][0]["Sales Product"]
+    this.purchase= data["default"][1]["Box"]
+    this.Product= data["default"][2]["Product Planning"]
+    // console.log(data["default"])
+   
   }
 
 }
